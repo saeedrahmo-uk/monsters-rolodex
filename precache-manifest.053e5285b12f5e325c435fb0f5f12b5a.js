cffe9ab1ed18f8b718705ee8b808c713b4f650cb
saeed rahmani <saeedrahmani@yandex.com>
2020-03-26 17:50:45 +0430

    Updates

---
+++ precache-manifest.053e5285b12f5e325c435fb0f5f12b5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b140ba3827c47128dd00c706357f85b0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a5bc8b49282efc987726",
+    "url": "/monsters-rolodex/static/css/main.b1ea9901.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5bc8b49282efc987726",
+    "url": "/monsters-rolodex/static/js/main.0249e434.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
